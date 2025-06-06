9a3ef3a73020286e0f1aa0d6f7013c08e17f3773
client <shin20281@gmail.com>
2025-06-06 23:22:58 +0900

    Create server1.py

---
+++ server1.py
@@ -0,0 +1,16 @@
+import socket
+import datetime
+
+PORT = 50000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("", PORT))
+server.listen()
+
+while True:
+    client, addr = server.accept()
+    msg = str(datetime.datetime.now())
+    client.sendall(msg.encode("UTF-8"))
+    print(msg, "connected")
+    print(client)
+    client.close()
